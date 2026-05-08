e69a3a1e242c0c7a9dc287d258a1eb639743fd29
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2026-05-08 16:09:04 -0400

    Create Result.cs

---
+++ API/Result.cs
@@ -0,0 +1,56 @@
+﻿namespace API
+{
+    public class Result<T>
+    {
+        #region Variables
+        private bool isSuccess;
+        private T value;
+        private string error;
+        #endregion
+
+
+
+        #region Constructor Method
+        public Result(bool IsSuccess,
+                      T Value,
+                      string Error)
+            : base()
+        {
+            this.isSuccess = IsSuccess;
+            this.value = Value;
+            this.error = Error;
+        }
+        #endregion
+
+
+
+        #region Field
+        public T Value
+        {
+            get => this.value;
+        }
+
+        public bool IsSuccess
+        {
+            get => this.isSuccess;
+        }
+
+        public string Error
+        {
+            get => this.error;
+        }
+        #endregion
+
+
+
+        public static Result<T> Success(T Value)
+        {
+            return new Result<T>(true, Value, null!);
+        }
+
+        public static Result<T> Failure(string Error)
+        {
+            return new Result<T>(false, default!, Error);
+        }
+    }
+}
