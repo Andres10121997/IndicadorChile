851270ee64d653d49dbe5a2febdb647b4e5a6425
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2026-06-01 15:29:44 -0400

    Actualización
    Web\Controllers\CurrencyController.cs
    Web\Views\Currency\Search.cshtml
    Web\Views\Home\Index.cshtml
    Web\Views\Shared\Partial\_Navbar.cshtml
    Web\Views\Shared\Partial\Form\_GetCurrency.cshtml

---
+++ Web/Controllers/CurrencyController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+
+namespace Web.Controllers
+{
+    public class CurrencyController : BaseController
+    {
+        #region Interfaces
+        private readonly ILogger<CurrencyController> logger;
+        #endregion
+
+
+
+        #region Constructor Method
+        public CurrencyController(ILogger<CurrencyController> Logger)
+            : base()
+        {
+            this.logger = Logger;
+        }
+        #endregion
+
+
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
