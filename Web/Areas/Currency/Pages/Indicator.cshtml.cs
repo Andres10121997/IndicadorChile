afd7586aff430e476b71c51cd24ed3def28f3bad
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2026-06-29 11:40:27 -0400

    Actualizaco+pm Indicador

---
+++ Web/Areas/Currency/Pages/Indicator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Areas.Currency.Pages
+{
+    public class IndicatorModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
