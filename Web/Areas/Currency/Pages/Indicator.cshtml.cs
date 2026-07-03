dcc9a392b377ca455d250d85881c570ef706c6b4
Andres10121997 <48809220+Andres10121997@users.noreply.github.com>
2026-07-03 10:04:29 -0400

    Actualización

--- Web/Areas/Currency/Pages/Indicator.cshtml.cs
+++ Web/Areas/Currency/Pages/Indicator.cshtml.cs
@@ -1,12 +1,28 @@
+using DTO.Currency;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Web.Areas.Currency.Pages
 {
     public class IndicatorModel : PageModel
     {
+        #region Variables
+        private CurrencyDto<float> currency;
+        #endregion
+
+
+
         public void OnGet()
         {
-
+            
         }
+
+
+
+        #region Field
+        public CurrencyDto<float> Currency
+        {
+            get => this.currency;
+        }
+        #endregion
     }
 }
